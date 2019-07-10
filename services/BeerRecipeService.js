e6383cc9d48cc222f6ccc1354d899cead1918cd8
Marce Ramirez <marceram87@gmail.com>
2019-07-10 11:42:50 +0200

    Connecto to API

--- services/BeerRecipeService.js
+++ services/BeerRecipeService.js
@@ -1,1 +1,14 @@
 'use strict';
+
+function BeerRecipeService() {
+  this.baseUrl = 'https://api.punkapi.com/v2/';
+}
+
+BeerRecipeService.prototype.getBeerRecipes = async function() {
+  var res = await fetch(`${this.baseUrl}/beers`);
+  var data = await res.json();
+};
+
+var beerServiceInstance = new BeerRecipeService();
+
+beerServiceInstance.getBeerRecipes();
