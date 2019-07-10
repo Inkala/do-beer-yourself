8b240f98d96e5b23bb1cae070a0de535d2b16fc5
Marce Ramirez <marceram87@gmail.com>
2019-07-10 16:20:53 +0200

    Create Layout component

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 'use strict';
+
+function main () {
+  var ENTRY_POINT = '/';
+  var layoutInstance = null;
+  var navbarInstance = null;
+  var rootElement = document.querySelector('#root');
+  
+  generateLayout();
+  generateNavbar();
+
+  function generateLayout() {
+    layoutInstance = new Layout(rootElement);
+    layoutInstance.generate();
+    console.log('layoutInstance', layoutInstance.header);
+  }
+  
+  function generateNavbar() {
+    navbarInstance = new Navbar(layoutInstance.header);
+    navbarInstance.generate();
+  }
+}
+
+window.addEventListener('load', main);
--- services/BeerRecipeService.js
+++ services/BeerRecipeService.js
@@ -1,12 +1,13 @@
 'use strict';
 
 function BeerRecipeService() {
-  this.baseUrl = 'https://api.punkapi.com/v2/';
+  this.baseUrl = 'https://api.punkapi.com/v2';
 }
 
 BeerRecipeService.prototype.getBeerRecipes = async function() {
   var res = await fetch(`${this.baseUrl}/beers`);
   var data = await res.json();
+  console.log(data);
 };
 
 var beerServiceInstance = new BeerRecipeService();
