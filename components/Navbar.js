7852a298ad9301cc0eef950930a98fa4fee88722
Marce Ramirez <marceram87@gmail.com>
2019-07-10 16:21:30 +0200

    Create Navbar component

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,1 +1,22 @@
 'use strict';
+
+function Navbar(root) {
+  this.root = root;
+  this.elements = null;
+  this.links = null;
+}
+
+Navbar.prototype.generate = function() {
+  this.elements = `
+    <nav class="site-navbar">
+      <a href="#0">
+        <img src="./images/dby-logo.png" alt="Do Beer Yourself Logo">
+      </a> 
+    </nav>
+  `;
+  this.render();
+}
+
+Navbar.prototype.render = function() {
+  this.root.innerHTML = this.elements;
+}
